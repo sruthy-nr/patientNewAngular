67593b35a4df1fc19daf822b90947cf31f691687
sruthy-nr <sruthyn.@nestgroup.net>
2022-12-08 14:17:50 +0530

    deletedata

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { ApiService } from '../api.service';
 
 @Component({
@@ -7,7 +8,7 @@ import { ApiService } from '../api.service';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent {
-constructor(private api:ApiService){}
+constructor(private api:ApiService,private route:Router){}
 name=""
 searchdata:any=[]
 readValues=()=>{
@@ -27,5 +28,26 @@ readValues=()=>{
       }
     }
   )
+}
+deletebuttonclick=(id:any)=>{
+
+  let data:any={
+    "id":id
+  }
+  this.api.deletePatient(data).subscribe(
+    (response:any)=>{
+      console.log(response)
+      if (response.status=="success") {
+
+        alert("Deleted");
+        this.route.navigate(['/view'])
+      } 
+      else {
+      
+        alert("invalid input");
+      }
+    }
+  )
+
 }
 }
